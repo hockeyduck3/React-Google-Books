059a2c52ee3df457e4c782a2953726c1a404a910
LJ Spencer <hockeyduck3@gmail.com>
2020-07-30 22:23:58 -0600

    Added mongoose model

---
+++ model/book.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Scehma = mongoose.Schema;
+
+const bookSchema = new Scehma({
+    title: {
+        type: String,
+        required: true 
+    },
+
+    authors: {
+        type: Array,
+        required: true
+    },
+
+    description : {
+        type: String
+    },
+
+    image: {
+        type: String
+    },
+
+    link: {
+        type: String
+    } 
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
