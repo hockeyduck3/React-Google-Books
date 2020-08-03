e27e04188df939a1612ae65537e83fd37cf227af
LJ Spencer <hockeyduck3@gmail.com>
2020-08-02 18:23:15 -0600

    Added some starter code for the search page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Nav from './components/Nav/index';
+import SearchPage from './pages/SearchPage';
 
 function App() {
     return (
         <Router>
             <div>
                 <Nav />
+                <Switch>
+                    <Route exact path='/' component={SearchPage}/>
+                </Switch>
             </div>
         </Router>
     )
---
+++ client/src/pages/SearchPage.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+    state = {
+
+    }
+
+    inputFunc = () => {
+
+    }
+
+    render() {
+        return (
+            <h1>Hello There!</h1>
+        )
+    }
+}
+
+export default Search;
