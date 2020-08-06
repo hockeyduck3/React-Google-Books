b491c67a236b764806519c9c1aef8ee83c30b072
LJ Spencer <hockeyduck3@gmail.com>
2020-08-05 18:03:36 -0600

    Fixing last mistake

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,4 @@ const apiRoutes = require('./api-routes');
 
 router.use('/api', apiRoutes);
 
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
-
 module.exports = router;
